ac2f694c888504e4c4d91a0d2a0547a53b17dd69
Wilson Shirado <wilson.shirado@animaeducacao.com.br>
2021-08-19 17:27:34 -0300

    refactor: Refatoracão das classes de Desempenho e ReajusteService

--- src/models/Desempenho.java
+++ src/models/Desempenho.java
@@ -1,7 +1,24 @@
 package models;
 
 public enum Desempenho {
-    A_DESEJAR ,
-    BOM,
-    OTIMO;    
+    A_DESEJAR {
+        @Override
+        public double percentualReajuste() {
+            return 0.03;
+        }
+    },
+    BOM {
+        @Override
+        public double percentualReajuste() {
+            return 0.15;
+        }
+    },
+    OTIMO {
+        @Override
+        public double percentualReajuste() {
+            return 0.20;
+        }
+    };
+
+    public abstract double percentualReajuste();
 }
--- src/services/ReajusteService.java
+++ src/services/ReajusteService.java
@@ -5,21 +5,7 @@ import models.Funcionario;
 
 public class ReajusteService {
     public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-        double reajuste;
-        switch(desempenho){
-            case A_DESEJAR:
-                reajuste = 0.03;
-                break;
-            case BOM:
-                reajuste = 0.15;
-                break;
-            case OTIMO:
-                reajuste = 0.20;
-                break;
-            default:
-                reajuste = 0;
-                break;
-        }        
+        double reajuste = desempenho.percentualReajuste();
         funcionario.reajustarSalario(reajuste);
     }
 }
