59ce5808ee4976d8e8c0965dc95b7b0c2bd56737
Wilson Shirado <wilson.shirado@animaeducacao.com.br>
2021-08-19 19:18:10 -0300

    chore: adição de banco de dados SQLite

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+
+import database.SQLiteConnection;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Main.main()");
+        SQLiteConnection.connect();
     }
     
 }
