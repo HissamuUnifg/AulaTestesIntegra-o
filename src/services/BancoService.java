f7b27e44b1f3ccda5c9ccd467837d4642af941bb
HissamuUnifg <68288195+HissamuUnifg@users.noreply.github.com>
2021-08-22 07:37:16 -0300

    Update BancoService.java

--- src/services/BancoService.java
+++ src/services/BancoService.java
@@ -2,7 +2,16 @@ package services;
 
 public class BancoService {
     public boolean pagarFuncionario(String conta, double salario){
-        System.out.println("Pagamento do funcionario " + conta + " efetuado. Valor: " + salario);
-        return true;
+        System.out.println("Coletando dados do funcionário...");
+        System.out.println("Validando credenciais com a API do banco via conexão https...");
+        System.out.println("Aguardando validação...");
+        System.out.println("verificando retorno...");
+        
+        if(true){
+            System.out.println("Pagamento do funcionario " + conta + " efetuado. Valor: " + salario);
+            return true;
+        }
+        System.out.println("Erro ao efetuar o pagamento.");
+        return false;
     }
 }
